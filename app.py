4073e0056221885db88a093c0f03244b345fb269
shelton111 <smatsinhe@gmail.com>
2024-06-20 17:53:42 +0200

    teste34

--- app.py
+++ app.py
@@ -2975,8 +2975,8 @@ def gerar_pdf(id):
 
 if __name__ == '__main__':
     app.secret_key='secret123'
-    app.run(debug=True)
-    #http_server = WSGIServer(('', 5000), app)
-    #http_server.serve_forever()
+    #app.run(debug=True)
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
     
 
